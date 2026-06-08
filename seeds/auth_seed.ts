3374c745f1e30594197807aec3db799da9fb2518
febryprasetyo <32359566+febryprasetyo@users.noreply.github.com>
2026-06-08 11:02:43 +0700

    Delete seeds directory

--- seeds/auth_seed.ts
+++
@@ -1,8 +0,0 @@
-import { Knex } from "knex";
-
-export async function seed(knex: Knex): Promise<void> {
-    // Inserts seed entries
-    await knex("users").insert([
-        { id: 1, username: "admin", password: "1234", role: "admin" }
-    ]).onConflict("username").ignore();
-};
